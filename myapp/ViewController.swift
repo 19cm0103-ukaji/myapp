714a3b920e6f1fccddf4db06e3437b5a7b1393e6
ukaji <19cm0103@jec.ac.jp>
2020-12-01 10:48:27 +0900

    Initial Commit

---
+++ myapp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myapp
+//
+//  Created by cmStudent on 2020/12/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
